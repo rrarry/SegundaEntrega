3f6ec6bca6b4e0bbfbf113dd729e9fa066bc4239
PedroCharpinel <pedrocharpinel123@usp.br>
2025-12-05 17:56:41 -0300

    Adiciona links de login/logout e perfil no header

--- blog_eletronica/urls.py
+++ blog_eletronica/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('posts.urls')),
 ]
